d4c2e91bb8de42fff255b69d1471390468fe4558
marconipoveda <marconipoveda84@gmail.com>
2020-07-15 21:48:21 -0600

    Triva con 2/5 preguntas

---
+++ reto3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Script de resolución reto 3
+# Solicitar que se ingrese un valor del 1 al 5. Según el valor ingresado valor a validarlo utilizando las condicionales e imprimir según el valor ingresado. Construir expresiones de validaciòn númericas, cadenas y archivos según la opción ingresada.
+option=""
+
+echo "Bienvenido a la resolución del reto!"
+echo "Elija su Opcion del siguente menú"
+echo "1) Pregunta de Geografia"
+echo "2) Pregunta de Historia"
+echo "3) Pregunta de Ciencias"
+echo "4) Pregunta de Política"
+echo "5) Pregunta de Informática"
+read -n1 -p "Escriba el número de su elección: " option
+
+echo -e "\n$option"
+
+case $option in
+	1) read -p "¿Cuál es la capital de Nueva Zelanda? " answerCapital
+		if [ $answerCapital = "Wellington" ]; then
+			echo "Excelente! Respuesta correcta!"
+		else
+			echo "Lo siento, $answerCapital no es la Capital de Nueva Zelanda!"
+		fi
+		;;
+	2) read -p "¿Cuántos años duró la guerra de los 100 años? " answerGuerra
+		if [ $answerGuerra -eq 116 ]; then
+			echo "Excelente! Respuesta correcta!"
+		else
+			echo "Lo siento, la Guerra de los 100 años no duró $answerGuerra años!"
+		fi
+		;;
+
+esac
